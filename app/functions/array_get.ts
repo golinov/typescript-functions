a07ebe6e939d2a5f87f0520fe1e5da907cd38496
Alexandr <golinov95@gmail.com>
2021-06-14 14:21:19 +0300

    fixed get by path

--- app/functions/array_get.ts
+++ app/functions/array_get.ts
@@ -1,4 +1,8 @@
 module.exports = function (arr: any[], path: string): any {
+    path = path.charAt(0) === '['
+        ? path
+        : `.${path}`
+
     try {
         return eval(`arr${path}`)
     } catch (e) {
